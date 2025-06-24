458cbd270eba5ad4d495c6f45d36498df0f87c1c
Guilherme-Augusto <guilhermex12g@gmail.com>
2025-06-23 21:52:48 -0300

    Update recuperaçao.sql

--- recuperaçao.sql
+++ recuperaçao.sql
@@ -7,26 +7,25 @@ use AppCrud;
 
 drop table Usuario;
 create table if not exists Usuario(
-	Id_usuario int primary key	auto_increment,
-    nome varchar(100) not null,
+	Id_usuario int primary key auto_increment,
+    	nome varchar(100) not null,
 	email varchar(100) not null,
-    senha varchar(100) not null
+    	senha varchar(100) not null
 )char set utf8mb4
 collate utf8mb4_unicode_ci;
 
 drop table tarefas;
 create table if not exists tarefas(
 	Id_tarefas int primary key auto_increment,
-    nome varchar(100) not null,
-    data_entrega date not null,
+    	nome varchar(100) not null,
+    	data_entrega date not null,
 	horario time,
-    descriçao varchar(100) not null,
-    Id_usuario int,
-    foreign key (Id_usuario) references Usuario(Id_usuario)
+    	descriçao varchar(100) not null,
+    	Id_usuario int,
+foreign key (Id_usuario) references Usuario(Id_usuario)
 ) char set utf8mb4
 collate utf8mb4_unicode_ci;
 
 select * from Usuario;
 select * from tarefas;
-DELETE FROM appcrud.usuario where Id_usuario > 0;
 
